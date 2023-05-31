2141b30b22662b04f59fcd316d3d16a2dc9802c8
meta8bits <116422521+meta8bits@users.noreply.github.com>
2023-05-31 16:20:11 +0000

    Setting version to 0.6.1-SNAPSHOT

--- infra.tf
+++ infra.tf
@@ -1,1 +1,27 @@
-ter
+terraform {
+
+  backend "s3" {
+    bucket = "my-sites-terraform-remote-state"
+    key    = "calcount-state"
+    region = "us-east-2"
+  }
+
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.7.1"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.4.1"
+    }
+  }
+}
+
+provider "kubernetes" {
+  config_path = "~/.kube/config"
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kub
