99a0133d30248350db51e4a293826c6c0c4e5bbf
meta8bits <116422521+meta8bits@users.noreply.github.com>
2023-06-05 06:14:06 +0000

    minor README fix

--- migrations/1_initdb.sql
+++ migrations/1_initdb.sql
@@ -3,4 +3,12 @@ create extension if not exists "uuid-ossp";
 create table users(
     id serial primary key,
     username varchar(255) unique not null,
-    email varchar(255) unique not
+    email varchar(255) unique not null,
+    salt varchar(255) not null,
+    digest varchar(255) not null
+);
+
+create table item(
+    id serial primary key,
+    name varchar(255) not null
+);
