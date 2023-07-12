eef3c53a77feffabbe1b54fba7e0922ddacdf41c
meta8bits <116422521+meta8bits@users.noreply.github.com>
2023-07-12 12:07:23 +0000

    Delete README.MD

--- src/auth/authenticate.rs
+++ src/auth/authenticate.rs
@@ -1,1 +1,13 @@
-//! Glue which integrates [crate::pw], [crate::db_ops], and [crate::ses
+//! Glue which integrates [crate::pw], [crate::db_ops], and [crate::session].
+//! Auth will authenticate users by fetching user info from the database and
+//! authenticating a user with the provided credentials.
+
+use super::{pw, Session};
+use crate::{
+    db_ops, db_ops::DbModel, models, preferences::get_user_preference,
+};
+use anyhow::{bail, Result};
+use chrono::Utc;
+use sqlx::{postgres::PgPool, query_as};
+
+/// We are a bit losey goosey on the identifier for a better u
