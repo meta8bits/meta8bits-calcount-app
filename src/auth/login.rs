eb2f5e5e8cf84c6610367e7508592197ea43eea5
meta8bits <116422521+meta8bits@users.noreply.github.com>
2023-07-21 09:38:44 +0000

    Updated csproj

--- src/auth/login.rs
+++ src/auth/login.rs
@@ -9,4 +9,13 @@ impl Component for LoginForm {
         let password_reset = Route::PasswordReset;
         format!(
             r#"
-     
+            <form class="flex flex-col gap-2 max-w-md" hx-post="{login_route}">
+                <h1 class="text-xl">Login to Bean Count</h1>
+                <label autocomplete="username" for="identifier">
+                    Username or Email
+                </label>
+                <input
+                    type="text"
+                    id="identifier"
+                    name="identifier"
+          
