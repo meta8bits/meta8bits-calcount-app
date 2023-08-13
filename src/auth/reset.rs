79eac0b0246616e2fad0655a4e590c67c26194be
meta8bits <116422521+meta8bits@users.noreply.github.com>
2023-08-13 16:35:50 +0000

    Update qbs submodule

--- src/auth/reset.rs
+++ src/auth/reset.rs
@@ -15,4 +15,11 @@ impl Component for ResetRequestForm {
             r#"
             <form hx-post="{reset_route}" class="flex flex-col gap-2 max-w-prose p-2 sm:p-4 md:p-8">
                 <h1 class="text-xl font-extrabold">Password Reset</h1>
-      
+                <label for="email">Email Address</label>
+                <p class="text-xs">
+                    Input the email associated with your account
+                </p>
+                <input type="email" id="email" name="email" required />
+                <button class="self-start dark:bg-green-700 dark:hover:bg-green-600 bg-green-100 hover:bg-green-200 rounded p-1">
+                    Submit
+                <
