65f5bf7f3529808069246181334736e768424642
meta8bits <116422521+meta8bits@users.noreply.github.com>
2023-08-13 12:06:14 +0000

    Version bump to 0.7.3

--- src/auth/reset.rs
+++ src/auth/reset.rs
@@ -1,4 +1,18 @@
 use super::pw;
 use crate::{
     auth,
-    config::{DOMAIN, RESET_TOKEN_T
+    config::{DOMAIN, RESET_TOKEN_TIMEOUT_MINUTES},
+    htmx,
+    prelude::*,
+    smtp::send_email,
+};
+
+struct ResetRequestForm;
+impl Component for ResetRequestForm {
+    fn render(&self) -> String {
+        let reset_route = Route::PasswordReset;
+        format!(
+            r#"
+            <form hx-post="{reset_route}" class="flex flex-col gap-2 max-w-prose p-2 sm:p-4 md:p-8">
+                <h1 class="text-xl font-extrabold">Password Reset</h1>
+      
