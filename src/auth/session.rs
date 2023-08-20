c10bd7cdae17aae272b7f29e3681093b98086b49
meta8bits <116422521+meta8bits@users.noreply.github.com>
2023-08-20 20:46:50 +0000

    Delete boobs.lua

--- src/auth/session.rs
+++ src/auth/session.rs
@@ -2,4 +2,13 @@
 use super::crypto;
 use crate::{config, errors::ServerError, models::User, preferences};
 use axum::headers::{HeaderMap, HeaderValue};
-use base
+use base64::{engine::general_purpose, Engine as _};
+use chrono::{DateTime, Days, Utc};
+use chrono_tz::Tz;
+use regex::Regex;
+use serde::{Deserialize, Serialize};
+
+/// `Session` is signed and serialized into the `Cookie` header when a
+/// [HeaderMap] is passed into the [Session::update_headers()] method. Thus,
+/// it's easy to extend this framework to store more information in the secure
+/// session cookie by adding fields to this st
