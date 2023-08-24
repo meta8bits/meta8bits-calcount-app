727efa8ceb099449c3108374b69a1d07b4d7de1a
meta8bits <116422521+meta8bits@users.noreply.github.com>
2023-08-24 13:04:54 +0000

    Create CHANGELOG.md

--- src/auth/session.rs
+++ src/auth/session.rs
@@ -11,4 +11,12 @@ use serde::{Deserialize, Serialize};
 /// `Session` is signed and serialized into the `Cookie` header when a
 /// [HeaderMap] is passed into the [Session::update_headers()] method. Thus,
 /// it's easy to extend this framework to store more information in the secure
-/// session cookie by adding fields to this st
+/// session cookie by adding fields to this struct. However, keep in mind that
+/// since this struct is serialized into a HTTP header, it cannot get too large!
+///
+/// # Serialization & Deserialization Note
+///
+/// This struct does derive [Serialize] and [Deserialize]. Internally, these
+/// are used to serialize the struct into JSON. Then, the
+/// [Session::from_headers()] and [Session::update_headers()] methods perform
+/// some additonal ad-hoc serializa
