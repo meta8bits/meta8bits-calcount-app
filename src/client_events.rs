a89c139b5525b95fdce5dfc56c66188fea290168
meta8bits <116422521+meta8bits@users.noreply.github.com>
2023-09-12 07:14:34 +0000

    fix fix fix

---
+++ src/client_events.rs
@@ -0,0 +1,9 @@
+use crate::htmx::trigger_event;
+use axum::headers::HeaderMap;
+
+pub fn reload_macros(headers: HeaderMap) -> HeaderMap {
+    trigger_event(headers, "reload-macros")
+}
+
+pub fn reload_meals(headers: HeaderMap) -> HeaderMap {
+    trigger_ev
