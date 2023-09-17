e0b4c2ae1cc0626e61fdd7b50e705858182f9851
meta8bits <116422521+meta8bits@users.noreply.github.com>
2023-09-17 05:26:30 +0000

    USB: OHCI: Allow runtime PM without system sleep

--- src/count_chat/demo.rs
+++ src/count_chat/demo.rs
@@ -9,4 +9,20 @@ impl Component for DemoMealOptions {
         format!(
             r##"
             <button
-                class="bg-green-100 p-1 r
+                class="bg-green-100 p-1 rounded shadow hover:bg-green-200"
+                hx-target="#cal-chat-container"
+                hx-get="{demo}"
+            >Reset</button>
+            "##
+        )
+    }
+}
+
+pub struct ChatDemo<'a> {
+    pub prefill_prompt: Option<&'a str>,
+}
+impl Component for ChatDemo<'_> {
+    fn render(&self) -> String {
+        let options = [
+            "5 second squeeze of honey",
+         
