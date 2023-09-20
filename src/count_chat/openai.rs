ccb0d6fdc51e4f277fdc7d03b5691df9631037c0
meta8bits <116422521+meta8bits@users.noreply.github.com>
2023-09-20 15:06:27 +0000

    v1.3.5

--- src/count_chat/openai.rs
+++ src/count_chat/openai.rs
@@ -11,4 +11,30 @@ pub struct OpenAI {
 
 #[derive(Serialize)]
 struct ChatCompletionRequest {
-    model: String
+    model: String,
+    messages: Vec<ChatCompletionMessage>,
+}
+
+#[derive(Serialize)]
+struct ChatCompletionMessage {
+    role: MessageRole,
+    content: String,
+}
+
+#[derive(Serialize)]
+#[allow(non_camel_case_types)]
+enum MessageRole {
+    user,
+    system,
+}
+
+#[derive(Deserialize)]
+struct ChatCompletionResponse {
+    choices: Vec<ChatCompletionResponseMessage>,
+    usage: Usage,
+}
+
+#[derive(Deserialize)]
+pub struct Usage {
+    pub prompt_tokens: i32,
+    pub completio
