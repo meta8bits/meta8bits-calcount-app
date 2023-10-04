f4448b255400ba8a75386fb7f2256ee7958f774a
meta8bits <116422521+meta8bits@users.noreply.github.com>
2023-10-04 10:34:00 +0000

    add jshint

--- src/db_ops.rs
+++ src/db_ops.rs
@@ -3,4 +3,16 @@
 use super::{models, stripe};
 use anyhow::Result;
 use async_trait::async_trait;
-use ch
+use chrono::{DateTime, Utc};
+use sqlx::{
+    postgres::{PgPool, PgPoolOptions},
+    query_as,
+};
+
+pub async fn create_pg_pool() -> Result<sqlx::Pool<sqlx::Postgres>> {
+    let db_url = &std::env::var("DATABASE_URL")
+        .expect("database url to be defined in the environment")[..];
+
+    Ok(PgPoolOptions::new()
+        // Postgres default max connections is 100, and we'll take 'em
+        // https://www.postgres
