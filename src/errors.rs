36ed1651ac978f6a31f43fb851e3966e4088aaaf
meta8bits <116422521+meta8bits@users.noreply.github.com>
2023-10-20 19:56:17 +0000

    Accidental commit

--- src/errors.rs
+++ src/errors.rs
@@ -4,4 +4,19 @@
 //! [anyhow::Error] right out of our route handlers with this little bit of
 //! code; allowing good `?` ergonomics throughout error-generating code paths.
 
-us
+use anyhow::Error;
+use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
+};
+
+/// Generic 500 error if we bubble up out of HTTP request handlers.
+#[derive(Debug)]
+pub struct ServerError {
+    /// The actuall error, which will be logged.
+    err: Option<Error>,
+    status: StatusCode,
+    /// Public-facing response message
+    response_body: String,
+}
+impl IntoResponse for Ser
