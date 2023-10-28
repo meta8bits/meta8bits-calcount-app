337dfaa3429e8ff5e965b4b129c0c009b71b3a5c
meta8bits <116422521+meta8bits@users.noreply.github.com>
2023-10-28 12:33:14 +0000

    up ver

--- src/legal/privacy_policy.rs
+++ src/legal/privacy_policy.rs
@@ -7,4 +7,10 @@ impl Component for PrivacyPolicy {
         <div class="prose bg-slate-300 rounded p-2 md:p-4">
             <h1>Privacy Policy</h1>
             <p>
-                
+                Information about all the food you eat is extremely personal
+                and private data. It is extremely important to protect your
+                privacy and data security. Your Bean Count data will never
+                be shared with a 3rd party under and circumstances, which
+                includes our email list as well as meal data you've entered
+                into the app.
+            <
