279bd27e0dd3df4d0f1cc7c449a430451086a04b
meta8bits <116422521+meta8bits@users.noreply.github.com>
2023-11-02 19:59:34 +0000

    Deploy mozilla/activate.mozilla.community to github.com/mozilla/activate.mozilla.community.git:gh-pages

--- src/preferences/user.rs
+++ src/preferences/user.rs
@@ -14,4 +14,19 @@ pub struct UserPreference {
     pub timezone: Tz,
     pub caloric_intake_goal: Option<i32>,
 }
-impl Default for UserPrefe
+impl Default for UserPreference {
+    fn default() -> Self {
+        Self {
+            timezone: Tz::UTC,
+            caloric_intake_goal: None,
+        }
+    }
+}
+
+impl Component for UserPreference {
+    fn render(&self) -> String {
+        let tz = self.timezone;
+        let goal = self
+            .caloric_intake_goal
+            .map_or("".to_string(), |g| g.to_string());
+        let options = TZ_VARIANTS.iter().fold(String::
