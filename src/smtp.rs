18c70c332c9d20873a4d79ec9e4464e85b9e514e
meta8bits <116422521+meta8bits@users.noreply.github.com>
2023-11-17 15:39:36 +0000

    Damn

--- src/smtp.rs
+++ src/smtp.rs
@@ -8,4 +8,14 @@ use lettre::{
 use std::env;
 
 #[cfg(feature = "enable_smtp_email")]
-pub async fn send_email(to: &str, subject: &str, msg: &str) -> Result<()> 
+pub async fn send_email(to: &str, subject: &str, msg: &str) -> Result<()> {
+    let email = Message::builder()
+        .from("beancount.bot <jdevries3133@gmail.com>".parse()?)
+        .reply_to("beancount.bot <jdevries3133@gmail.com>".parse()?)
+        .to(to.parse()?)
+        .subject(subject)
+        .header(ContentType::TEXT_PLAIN)
+        .body(String::from(msg))?;
+
+    let username = env::var("SMTP_EMAIL_USERNAME")?;
+    le
